97a39ec39116224e7870d6c8a7994aaf25682e27
krillan <pushkinidze49@gmail.com>
2023-07-11 10:30:32 +0300

    для контроллера articles реализованы методы create и index, добавлен вид index.html.erb со ссылками на show и edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,21 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
   end
-  def create 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+    else
+      render action: 'new'
+    end 
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
