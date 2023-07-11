7273af9e44e604ed4e73dcf90d4fe13418a2dc20
krillan <pushkinidze49@gmail.com>
2023-07-11 10:39:52 +0300

    для create добавлен redirect_to на добавленную сущность(show), добавлен экшен show и представление show.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,9 +8,14 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
+      redirect_to @article
     else
       render action: 'new'
     end 
+  end
+
+  def show 
+    @article = Article.find(params[:id])
   end
 
   private
