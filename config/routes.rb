1f6cad9518a44eef5a0aeea71da767eb4f92d604
krillan <pushkinidze49@gmail.com>
2023-07-11 11:05:44 +0300

    добавлен контроллер pages для статических страниц, экшены about и terms, представления и маршруты от корня для них

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
 
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
   resources :articles
   
   get 'contacts' => 'contacts#new'
