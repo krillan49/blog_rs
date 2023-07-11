f9cd24b9eb06274a00fefffe986a11e85719069a
krillan <pushkinidze49@gmail.com>
2023-07-11 08:08:16 +0300

    для contacts реализован метод create с валидацией, в представление new.html.erb добавлен список ошибок, так же добавлены стили для ошибок

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,5 +2,13 @@ class ContactsController < ApplicationController
   def new 
   end
   def create
+    @contact = Contact.new(contact_params)
+    render action: 'new' if !@contact.save
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/' => 'home#index'
 
   resources :articles
+  
   resource :contacts, only: [:new, :create]
 end
