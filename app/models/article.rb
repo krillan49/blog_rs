d4686d8b7b00edac4355103c308398faee1d8f72
krillan <pushkinidze49@gmail.com>
2023-08-04 11:02:36 +0300

    добавлен foreign key в articles к users и установлена связь один ко многим в моделях

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
   validates :text, presence: true
 
   has_many :comments
+  belongs_to :user
 end
---
+++ db/migrate/20230804075512_add_fk_article_to_user.rb
@@ -0,0 +1,5 @@
+class AddFkArticleToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :articles, :user
+  end
+end
