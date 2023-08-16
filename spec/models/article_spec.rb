0b6885720237f42e5db960d1cb6f11255a5f883e
krillan <pushkinidze49@gmail.com>
2023-08-16 09:33:17 +0300

    добавлена доп валидация в модели article и comment, добавлены тесты для проверки валидации этих моделей

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 describe Article do
-  it { should have_many :comments }
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+    it { should validate_length_of(:title).is_at_most(140) } # не больше чем 140
+    it { should validate_length_of(:text).is_at_most(4000) }
+  end
+
+  describe "assotiations" do
+    it { should have_many :comments }
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe Comment do
-  it { should belong_to :article }
+  describe "validations" do
+    it { should validate_length_of(:body).is_at_most(4000) }
+  end
+
+  describe "assotiations" do
+    it { should belong_to :article }
+  end
 end
