13fd795daf3d0edd548c2a5a4e86a0203d7bd120
krillan <pushkinidze49@gmail.com>
2023-08-21 10:54:27 +0300

    article creation test

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -7,6 +7,16 @@ feature "Article Creation" do
 
   scenario "allows user to visit new article page" do
     visit new_article_path
-    expect(page).to have_content I18n.t('articles.new.header') #I18n.t('articles.show.new_comment')
+    expect(page).to have_content I18n.t('articles.new.header')
+  end
+
+  scenario "allows user to visit new article page" do
+    visit new_article_path
+
+    fill_in :article_title, with: 'Test article'
+    fill_in :article_text, with: 'Some text'
+    click_button 'Save Article'
+
+    expect(page).to have_content I18n.t('articles.show.new_comment')
   end
 end
